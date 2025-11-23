56e078dab6a9e5e4c36fd2000d564c26dca6cd54
ccarolinaag <clara.garcia@sptech.school>
2025-11-23 18:57:18 -0300

    remocao(assets) Remove assets não utilizados no projeto

--- site/web-data-viz/src/models/usuarioModel.js
+++ site/web-data-viz/src/models/usuarioModel.js
@@ -40,7 +40,6 @@ WHERE u.id != '${id}';
 function adicionarLivro(nome, autor, numpaginas, editora, imagem, resenha, dtinicio, dttermino, fkusuario) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():");
 
-    console.log("iamgemmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm: " + imagem)
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
